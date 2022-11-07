1e9664c0a4834a0c8aeb9d829b52a9586b9c7409
gufransha <gufranshaikh242@gmail.com>
2022-11-07 14:39:50 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { TodosComponent } from './My-component/todos/todos.component';
+import { TodoItemComponent } from './My-component/todo-item/todo-item.component';
+import { AddTodoComponent } from './My-component/add-todo/add-todo.component';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    TodoItemComponent,
+    AddTodoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
